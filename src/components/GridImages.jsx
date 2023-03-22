494213d2a53f21812cb29a682f84671c9216b64c
Jabs69 <jbenitezsaez@gmail.com>
2023-03-21 21:06:07 -0400

    add source folder

---
+++ src/components/GridImages.jsx
@@ -0,0 +1,17 @@
+import {View,Text} from 'react-native';
+
+const GridImages = () => {
+
+	return (
+
+		<View>
+			
+			<Text>The images shoulbe be here</Text>
+
+		</View>
+
+	)
+
+}
+
+export default GridImages;
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import GridImages from './GridImages.jsx';
+
+const Main = () => {
+
+	return (
+
+		<GridImages/>
+
+	)
+
+}
+
+export default Main;
