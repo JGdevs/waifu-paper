1ce3870fa4582b445be180f355c7108cb4207879
Gabriel Saez <gabrielsaez2016@gmail.com>
2023-03-22 14:17:58 -0400

    add dark mode

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,48 @@
+import {View,Text,useColorScheme,StyleSheet} from 'react-native';
 import GridImages from './GridImages.jsx';
+import Header from './Header.jsx';
+
+const styles = StyleSheet.create({
+
+	default:{
+
+		flex:1,
+		color:'#101010',
+		backgroundColor:'#efefef'
+
+	},
+
+	dark:{
+
+		color:'#f9f9f9',
+		backgroundColor:'#030303'
+
+	}
+
+});
 
 const Main = () => {
 
+	const theme = useColorScheme(),
+
+	isDarkTheme = theme === 'dark',
+
+	mainStyles = [
+
+		styles.default,
+		isDarkTheme && styles.dark
+
+	];
+
 	return (
 
-		<GridImages/>
+		<View style={mainStyles}>
+			
+			<Header/>
+			<GridImages/>
+			<Text>{theme}</Text>
+
+		</View>
 
 	)
 
