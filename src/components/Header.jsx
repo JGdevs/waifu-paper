44c066677e359a9256ebb390716ca3caa7961494
Gabriel Saez <gabrielsaez2016@gmail.com>
2023-04-07 17:16:13 -0400

    finished the header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,44 @@ import {StyledText} from './StyledText.jsx';
 import Constants from 'expo-constants';
 import StyledView from './StyledView.jsx';
 import MenuIcon from './MenuIcon.jsx';
+import SearchIcon from './SearchIcon.jsx';
+import Tabs from './Tabs.jsx';
 
 const styles = StyleSheet.create({
 
 	header:{
 
-		alignItems:'center',
+		height:Constants.statusBarHeight + 20,
+		justifyContent:'center'
+
+	},
+
+	headerOptions:{
+
+		height:'50%',
+		flexDirection:'row',
 		justifyContent:'space-between',
-		paddingTop:Constants.statusBarHeight + 40
+		alignItems:'center',
+
+	},
+
+	headerTitle: {
+
+		flexDirection:'row',
+		width:'35%',
+		justifyContent:'space-around',
+
+	},
+
+	headerSearch: {
+
+		paddingRight:10
+
+	},
+
+	headerTabs:{
+
+
 
 	}
 
@@ -22,7 +52,24 @@ const Header = () => {
 
 		<StyledView style={styles.header} child>
 			
-			<MenuIcon/>
+			<View style={styles.headerOptions}>
+
+				<View style={styles.headerTitle}>
+						
+					<MenuIcon/>
+					<StyledText>WaifuPaper</StyledText>
+
+				</View>
+
+				<View style={styles.headerSearch}>
+					
+					<SearchIcon/>
+
+				</View>
+			
+			</View>
+
+			<Tabs/>
 
 		</StyledView>
 
