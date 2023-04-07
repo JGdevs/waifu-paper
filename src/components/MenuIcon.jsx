4c762a5ab240add39d0cb8d6b702b1b8e45f5ddf
Gabriel Saez <gabrielsaez2016@gmail.com>
2023-04-07 17:16:53 -0400

    Menu Icon add propertys

--- src/components/MenuIcon.jsx
+++ src/components/MenuIcon.jsx
@@ -4,10 +4,10 @@ const MenuIcon = () => {
 
 	return (	
 
-		<Svg viewBox="0 0 100 80" width="40" height="40">
-		   <Rect width="100" height="20" rx="8"/>
-		   <Rect y="30" width="100" height="20" rx="8"/>
-		   <Rect y="60" width="100" height="20" rx="8"/>
+		<Svg viewBox="0 0 100 80" width="20" height="20">
+		   <Rect width="100" height="10" rx="8" fill="#fff"/>
+		   <Rect y="30" width="100" height="10" rx="8" fill="#fff"/>
+		   <Rect y="60" width="100" height="10" rx="8" fill="#fff"/>
 		</Svg>
 
 	)
