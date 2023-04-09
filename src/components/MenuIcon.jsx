426bd4ffe0b8b9f26a1be994d4f1a84546d2c8b9
Gabriel Saez <gabrielsaez2016@gmail.com>
2023-04-09 15:35:32 -0400

    change propertys in MenuIcon

--- src/components/MenuIcon.jsx
+++ src/components/MenuIcon.jsx
@@ -4,7 +4,7 @@ const MenuIcon = () => {
 
 	return (	
 
-		<Svg viewBox="0 0 100 80" width="20" height="20">
+		<Svg viewBox="0 0 100 60" width="18" height="18">
 		   <Rect width="100" height="10" rx="8" fill="#fff"/>
 		   <Rect y="30" width="100" height="10" rx="8" fill="#fff"/>
 		   <Rect y="60" width="100" height="10" rx="8" fill="#fff"/>
