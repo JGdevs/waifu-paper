390f02cf4ea93fed8b540f3b89b2a19eb7dac634
Gabriel Saez <gabrielsaez2016@gmail.com>
2023-04-15 21:59:43 -0400

    new files added

---
+++ src/data/drawerItems.js
@@ -0,0 +1,52 @@
+export default [
+
+{
+
+	name:'WaifuPaper',
+	iconName:'home',
+	familyIcon:'Feather'
+
+},
+
+{
+
+	name:'About',
+	iconName:'alert-circle',
+	familyIcon:'Feather'
+
+},
+
+{
+
+	name:'Search',
+	iconName:'search',
+	familyIcon:'Feather'
+
+},
+
+{
+
+	name:'Saved',
+	iconName:'bookmark',
+	familyIcon:'FontAwesome'
+
+},
+
+{
+
+	name:'Rate',
+	iconName:'star',
+	familyIcon:'Feather'
+
+
+},
+
+{
+
+	name:'settings',
+	iconName:'settings',
+	familyIcon:'Feather'
+
+}
+
+]
