532217d7e6ce01e395ad5618933d935bc800e5db
Gabriel Saez <gabrielsaez2016@gmail.com>
2023-04-27 11:17:54 -0400

    using Tanstack Query to make the request

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import 'react-native-gesture-handler';
 import { StyleSheet, Text, View } from 'react-native';
 import Main from './src/components/Main.jsx';
+import {QueryClientProvider,QueryClient} from '@tanstack/react-query';
 
 export default function App() {
 
+  const queryClient = new QueryClient();
+
   return (
 
-    <Main/>
+    <QueryClientProvider client={queryClient}>
+      <Main/>
+    </QueryClientProvider>
 
   )
 }
