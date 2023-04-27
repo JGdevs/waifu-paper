14fc96b3fc7a9008d592ff6db607972e4e69a94e
Gabriel Saez <gabrielsaez2016@gmail.com>
2023-04-27 11:18:12 -0400

    changes in GridImages

--- src/components/GridImages.jsx
+++ src/components/GridImages.jsx
@@ -1,18 +1,76 @@
-import {View} from 'react-native';
+import {useCallback} from 'react';
+import {FlatList,StyleSheet,Image} from 'react-native';
 import {StyledText} from './StyledText.jsx';
+import StyledView from './StyledView.jsx';
+import {useQuery} from '@tanstack/react-query';
+import {getImages} from '../services/images.js';
 
 const GridImages = () => {
 
+	const {isLoading,isError,data:images = []} = useQuery ({
+
+		queryKey:['images'],
+		queryFn: async () => await getImages('default')
+
+	})
+
+	const styles = StyleSheet.create({
+
+		containerImages:{
+
+			
+
+		},
+
+		image:{
+
+			width:'48%',
+			height:300,
+			marginVertical:2,
+			marginHorizontal:2
+
+		}
+
+	})
+
+	const renderItem = useCallback(({item,index}) => (
+
+		<Image
+
+		 	style={{...styles.image,backgroundColor:item.dominant_color}} 
+		 	src={item.url}
+		 	resizeMode="cover"
+
+		/>
+
+	),[]);
+	
 	return (
 
-		<View>
+		<StyledView style={styles.containerImages}>
 			
+			{images.length > 1 && 
+
+				<FlatList
+
+					data={images}
+					renderItem={renderItem}
+					numColumns={2}
+					getItemLayout={(data, index) => ({length: 300, offset: 300 * index, index})}
+					keyExtractor={item => item.image_id}
+					removeClippedSubviews
+					initialNumToRender={6}
+
+				/>
+				
+			}
+
+			{isLoading && <StyledText>Loading</StyledText>}
 			
-			
-		</View>
+		</StyledView>
 
 	)
 
 }
 
-export default GridImages;
+export default GridImages; 
