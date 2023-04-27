083eef3d140e7a6ed48c0fd017ab3de22d645dfc
Gabriel Saez <gabrielsaez2016@gmail.com>
2023-04-27 11:17:26 -0400

    new changes in Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,53 +1,22 @@
-import {View,Text,useColorScheme,StyleSheet,StatusBar} from 'react-native';
+import {useColorScheme,StatusBar} from 'react-native';
 import GridImages from './GridImages.jsx';
 import Header from './Header.jsx';
-import {StyledText} from './StyledText.jsx';
 import {NavigationContainer} from '@react-navigation/native';
+import StyledView from './StyledView.jsx';
 import MenuPanel from './MenuPanel.jsx';
 
-
-const styles = StyleSheet.create({
-
-	default:{
-
-		flex:1,
-		color:'#101010',
-		backgroundColor:'#efefef'
-
-	},
-
-	dark:{
-
-		color:'#f9f9f9',
-		backgroundColor:'#030303'
-
-	}
-
-});
-
 const Main = () => {
-
-	const theme = useColorScheme(),
-
-	isDarkTheme = theme === 'dark',
-
-	mainStyles = [
-
-		styles.default,
-		isDarkTheme && styles.dark
-
-	];
 
 	return (
 
 		<NavigationContainer>	
 
-			<View style={mainStyles}>
+			<StyledView style={{flex:1}}>
 			
 				<StatusBar/>
 				<MenuPanel/>
 
-			</View>
+			</StyledView>
 
 		</NavigationContainer>	
 
